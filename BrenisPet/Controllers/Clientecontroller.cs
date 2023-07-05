5e55caf65e9b5797b607e7268bd7e0cdea158024
BrenisN <nahuelbrenis@gmail.com>
2023-07-05 20:32:55 -0300

    Entrega

---
+++ BrenisPet/Controllers/Clientecontroller.cs
@@ -0,0 +1,97 @@
+﻿using BrenisPet.Properties.NewFolder2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrenisPet.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class Clientecontroller : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+
+            List<cliente> clientes = new List<cliente>
+            { 
+                new cliente
+                {
+                    id = "1",
+                    correo = "google@gmail.com",
+                    edad = "19",
+                    nombre = "Bernardo"
+                },
+
+                 new cliente
+                {
+                    id = "2",
+                    correo = "brenis@gmail.com",
+                    edad = "19",
+                    nombre = "nahuel"
+                 }
+
+            };
+            return clientes; 
+         
+
+        }
+
+        [HttpGet]
+        [Route("listarxid")]
+        public IActionResult listarClientexid(int codigo)
+        {
+            //obtienes el cliente de la db
+
+            return Ok( new cliente
+            {
+                id = codigo.ToString(),
+                correo = "google@gmail.com",
+                edad = "19",
+                nombre = "Bernardo"
+            });
+
+
+        }
+
+
+        [HttpPost]
+        [Route("guardar")]
+        public IActionResult guardarCliente(cliente cliente)
+        {
+            //guardar en la db y le da un id
+            cliente.id = "3";
+            return Ok(new
+            {
+                success = true,
+                message = "cliente registrado",
+                result = cliente
+            });
+        }
+
+        [HttpPost]
+        [Route("eliminar")]
+        public dynamic eliminarCliente(cliente cliente)
+        {
+            string token = Request.Headers.Where(x => x.Key =="Autorization").FirstOrDefault().Value;
+
+            //elimina un cliente la db
+           
+            if(token != "marco123.")
+            {
+                return new
+                {
+                    success = false,
+                    message = "token incorrecto",
+                    result = ""
+                };
+            }
+
+            return new
+            {
+                success = true,
+                message = "cliente eliminado",
+                result = cliente
+            };
+        }
+    }
+}
